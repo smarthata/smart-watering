d0f0bfbdcf9c5c1fc8d6ac7b4ffac42b65c6ca6b
Valery Akhrymchuk <valeryakhrymchuk@gmail.com>
2017-04-06 20:49:48 +0300

    Add DS3231 clock

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <Arduino.h>
+#include <DS3231.h>
+
+DS3231 clock;
+bool h12 = false;
+bool PM = false;
 
 void setup() {
     Serial.begin(9600);
+    Wire.begin();
 }
 
 void loop() {
     Serial.println(millis());
-    delay(300);
+    Serial.print(clock.getHour(h12, PM));
+    Serial.print(':');
+    Serial.print(clock.getMinute());
+    Serial.print(':');
+    Serial.println(clock.getSecond());
+    delay(1000);
 }
